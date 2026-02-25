def20ef2b9ebdc027e2a4d2e49828dff2c952277
asenhali <misohali87@gmail.com>
2026-02-25 22:39:25 +0100

    UI: attach BottomNav to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,16 @@
+import BottomNav from "@/components/BottomNav";
+
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      {children}
-      {/* BottomNav placeholder */}
-    </>
+    <div className="app-shell">
+      <main className="content-area pb-[80px]">
+        {children}
+      </main>
+      <BottomNav />
+    </div>
   );
 }
